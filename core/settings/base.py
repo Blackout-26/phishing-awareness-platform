ff967eb64fbae172d5b10a939a0f25954beb6ca6
Blackout-26 <princengonyama26@gmail.com>
2026-03-30 12:36:36 +0200

    Overhauled UI to a modern Enterprise SaaS dark theme

--- core/settings/base.py
+++ core/settings/base.py
@@ -145,6 +145,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.resend.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'resend'
+EMAIL_HOST_USER = 'resend' 
 EMAIL_HOST_PASSWORD = os.getenv('RESEND_API_KEY')
 DEFAULT_FROM_EMAIL = 'onboarding@resend.dev'
